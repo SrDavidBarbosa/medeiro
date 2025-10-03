6f5f01ac8b69ec9d971abb2b7f643438be85e98f
David Barbosa <davidsantosbarbosa@hotmail.com>
2025-10-03 23:31:57 +0200

    update erros

--- src/pages/api/reindex.js
+++ src/pages/api/reindex.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
         const result = await index();
         res.status(200).send({ result });
     } catch (err) {
-        console.error(err);
-        res.status(500).send({ error: 'failed to fetch data' });
+        console.error('Error during reindexing:', err.stack);
+        res.status(500).send({ error: `Failed to fetch data: ${err.message}` });
     }
 }
